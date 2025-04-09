d3230ade9947234297b2f72168f3b84c549ce47e
hexstories <kwakuasihene@gmail.com>
2025-04-09 16:26:03 +0000

    pending commits

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );
+  define( 'DB_PASS', 'admin' );
+  define( 'DB_NAME', 'inventory_system' );
+
+?>
---
+++ layouts/admin_menu.php
@@ -0,0 +1,103 @@
+<ul>
+    <li>
+        <a href="admin.php" title="Dashboard">
+            <i class="glyphicon glyphicon-home"></i>
+            <span>Dashboard</span>
+        </a>
+    </li>
+
+    <li>
+        <a href="#" class="submenu-toggle" title="User Management">
+            <i class="glyphicon glyphicon-user"></i>
+            <span>User Management</span>
+        </a>
+        <ul class="nav submenu">
+            <li>
+                <a href="group.php">
+                    <i class="glyphicon glyphicon-cog"></i> Manage Groups
+                </a>
+            </li>
+            <li>
+                <a href="users.php">
+                    <i class="glyphicon glyphicon-user"></i> Manage Users
+                </a>
+            </li>
+        </ul>
+    </li>
+
+    <li>
+        <a href="categorie.php" title="Categories">
+            <i class="glyphicon glyphicon-indent-left"></i>
+            <span>Categories</span>
+        </a>
+    </li>
+
+    <li>
+        <a href="#" class="submenu-toggle" title="Product Management">
+            <i class="glyphicon glyphicon-th-large"></i>
+            <span>Products</span>
+        </a>
+        <ul class="nav submenu">
+            <li>
+                <a href="product.php">
+                    <i class="glyphicon glyphicon-list-alt"></i> Manage Products
+                </a>
+            </li>
+            <li>
+                <a href="add_product.php">
+                    <i class="glyphicon glyphicon-plus-sign"></i> Add Product
+                </a>
+            </li>
+        </ul>
+    </li>
+
+    <li>
+        <a href="media.php" title="Media Files">
+            <i class="glyphicon glyphicon-picture"></i>
+            <span>Media Files</span>
+        </a>
+    </li>
+
+    <li>
+        <a href="#" class="submenu-toggle" title="Sales Management">
+            <i class="glyphicon glyphicon-credit-card"></i>
+            <span>Sales</span>
+        </a>
+        <ul class="nav submenu">
+            <li>
+                <a href="sales.php">
+                    <i class="glyphicon glyphicon-list-alt"></i> Manage Sales
+                </a>
+            </li>
+            <li>
+                <a href="add_sale.php">
+                    <i class="glyphicon glyphicon-plus-sign"></i> Add Sale
+                </a>
+            </li>
+        </ul>
+    </li>
+
+    <li>
+        <a href="#" class="submenu-toggle" title="Sales Reports">
+            <i class="glyphicon glyphicon-duplicate"></i>
+            <span>Sales Report</span>
+        </a>
+        <ul class="nav submenu">
+            <li>
+                <a href="sales_report.php">
+                    <i class="glyphicon glyphicon-calendar"></i> Sales by Date Range
+                </a>
+            </li>
+            <li>
+                <a href="monthly_sales.php">
+                    <i class="glyphicon glyphicon-stats"></i> Monthly Sales
+                </a>
+            </li>
+            <li>
+                <a href="daily_sales.php">
+                    <i class="glyphicon glyphicon-time"></i> Daily Sales
+                </a>
+            </li>
+        </ul>
+    </li>
+</ul>
